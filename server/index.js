8e5f17a9eb9cd69c707b5c1d030e0573cf9c70c6
Guillermo San Martín <guille.seba0802@gmail.com>
2023-06-19 20:45:52 -0300

    prod config

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { Server } from 'socket.io';
 
 import http from 'http';
+import { PORT } from '../config.js';
 
 const app = express();
 const server = http.createServer(app);
@@ -20,6 +21,5 @@ io.on('connection', socket => {
     });
   });
 });
-
-server.listen(3000);
-console.log(`Server on port 3000`);
+server.listen(PORT);
+console.log(`Server on port ${PORT}`);
