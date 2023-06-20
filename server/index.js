cab394d4719c7d62f2c540009f60cd932fcef85f
Guillermo San Martín <guille.seba0802@gmail.com>
2023-06-19 22:28:07 -0300

    change frontend url

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://react-chatappsocket.netlify.app/',
+    origin: 'https://react-chatappsocket.netlify.app',
   },
 });
 
